1dcb5283d93841faed7c50950b8353ba5f596749
Fan Huang <fan.huang.33@gmail.com>
2019-01-06 21:27:50 -0500

    switched over to postgresql

--- kaiketsu/settings.py
+++ kaiketsu/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'kaiketsu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '127.0.0.1',
+        'NAME': 'kaiketsu',
+        'PORT': '5432',
     }
 }
 
