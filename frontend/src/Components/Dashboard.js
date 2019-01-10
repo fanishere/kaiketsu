4776cb38beee176dddea29210a8bf3e386d32fe7
Fan Huang <fan.huang.33@gmail.com>
2019-01-09 20:26:36 -0500

    goals show correctly on page

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -1,19 +1,49 @@
 import React, {Component} from 'react';
 import './Dashboard.css';
 import {connect} from "react-redux";
+import {
+    BrowserRouter,
+    Route,
+    Link,
+    NavLink
+} from "react-router-dom";
+import Loading from './Loading';
 const axios = require('axios');
 
 class GoalBlock extends Component {
     render() {
         return (
             <div className="GoalBlock">
+                <div className="resolution">
+                    <div className="goalblock-title">
+                        <h2>RESOLUTION</h2>
+                    </div>
+                    
+                    <p>{ this.props.resolution }</p>
+                </div>
+                
+                <div className="category">
+                    <div className="goalblock-title">
+                        <h2>CATEGORY</h2>
+                    </div>
+                    
+                    <p>{ this.props.category }</p>
+                </div>
+                <div className="reason">
+                    <div className="goalblock-title">
+                        <h2>REASON</h2>
+                    </div>
+                    
+                    <p>{ this.props.reason }</p>
+
+                </div>
                 
             </div>
         )
     }
 }
 
-class DashboardDisplay extends Component {
+class DashboardGoalDisplay extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -56,7 +86,7 @@ class DashboardDisplay extends Component {
                         resolution={ goals[i].resolution }
                         reason={ goals[i].reason}
                         duration={ goals[i].duration }
-                        category={ goals[i].duration }
+                        category={ goals[i].category }
                         key={i}
                         >
                     </GoalBlock>
@@ -79,8 +109,8 @@ class DashboardHeader extends Component {
         return (
             <div className="header">
                 <div className="tabs">
-                    <div><span>Goals</span></div>
-                    <div><span>Progress</span></div>
+                    <div><NavLink to="/dashboard/goals" activeClassName="selected">Goals</NavLink></div>
+                    <div><NavLink to="/dashboard/progress" activeClassName="selected">Progress</NavLink></div>
                 </div>
             </div>
         )
@@ -91,8 +121,28 @@ class Dashboard extends Component {
     render() {
         return (
             <div className="Dashboard">
-                <DashboardHeader></DashboardHeader>
-                <DashboardDisplay token={this.props.token}></DashboardDisplay>
+                <DashboardHeader token={this.props.token}></DashboardHeader>
+
+
+            
+            
+            <Route
+                                path="/dashboard/goals"
+                                render={(props) => <DashboardGoalDisplay {...props} token={this.props.token}/>}
+                            />
+                            <Route exact path="/dashboard/progress" component={Loading}/>
+                {/* <BrowserRouter>
+                    <div>
+                        <div className="content">
+                            <Route
+                                path="/dashboard/goals"
+                                render={(props) => <DashboardGoalDisplay {...props} token={this.props.token}/>}
+                            />
+                            <Route exact path="/dashboard/progress" component={Loading}/>
+
+                        </div>
+                    </div>
+                </BrowserRouter> */}
             </div>
             
         )
@@ -100,6 +150,7 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => {
+    console.log(state);
     let errors = [];
     if (state.auth.errors) {
         errors = Object.keys(state.auth.errors).map(field => {
