5b9b4de433f548bbeaff710da8f6afb1caa17f8e
Fan Huang <fan.huang.33@gmail.com>
2019-01-15 19:21:39 -0500

    switches time back

--- kaiketsu/tasks.py
+++ kaiketsu/tasks.py
@@ -29,7 +29,7 @@ def make_goal_day_false():
 app.conf.beat_schedule = {
     'make-false-checks': {
         'task': 'kaiketsu.tasks.make_goal_day_false',
-        'schedule': 10.0,
+        'schedule': crontab(hour=23, minute=45),
         'args': ()
     },
 }
