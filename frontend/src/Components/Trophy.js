e44069273c4bdfb611f0090bdd2b716537db4bd1
Fan Huang <fan.huang.33@gmail.com>
2019-01-21 19:01:53 -0500

    adds trophy page

--- frontend/src/Components/Trophy.js
+++ frontend/src/Components/Trophy.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux";
+import './Trophy.css';
+import personal_logo from './media/Monday/Balloon_logo.png';
+import health_logo from './media/Monday/Cactus_logo.png';
+import prof_logo from './media/Monday/Geo_logo.png';
 const axios = require('axios');
 
+
+class TrophyItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            phrases: [
+                'sdfsdf',
+                'mpopojrgpoejr',
+                'sdfsdf'
+            ]
+        }
+    }
+
+    getRandomPhrase() {
+        let phraseNumber = Math.floor(Math.random() * (this.state.phrases.length));
+        return this.state.phrases[phraseNumber];
+    }
+
+    render() {
+        return (
+            <div className="TrophyItem">
+                <div className="trophyImage">
+                    <img src={personal_logo} alt="personal logo"></img>
+                </div>
+                <div className="trophyData">
+                    <p>Congratulations! In your journey to &nbsp;
+                    {this.props.resolution.toLowerCase()} for {secondsToDays(this.props.duration)} days.</p>
+                    <p>{this.getRandomPhrase()}</p>
+                </div>
+            </div>
+        );
+    }
+}
 
 class Trophy extends Component {
     constructor(props) {
@@ -24,7 +61,7 @@ class Trophy extends Component {
             if (res.status === 200) {
                 console.log(res);
                 this.setState({
-                    goals: res.data
+                    data: res.data
                 })
                 return res.data;
 
@@ -36,12 +73,32 @@ class Trophy extends Component {
     }
 
     render() {
+        let trophyItems = [];
+        if (this.state.data) {
+            let goals = this.state.data;
+            for (let i = 0; i < goals.length; i++) {
+                trophyItems.push(
+                    <TrophyItem
+                        resolution={goals[i].resolution}
+                        reason={goals[i].reason}
+                        duration={goals[i].duration}
+                        category={goals[i].category}
+                        key={i}
+                    >
+                    </TrophyItem>
+                );
+            }
+
+        }
         return (
             <div className="Trophy">
-
+                {trophyItems}
             </div>
         );
     }
+}
+function secondsToDays(num) {
+    return num / 86400;
 }
 
 
