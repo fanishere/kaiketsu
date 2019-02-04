db42ed327265aae2d500466cd4d8a707c8cd83a7
Fan Huang <fan.huang.33@gmail.com>
2019-02-04 17:47:05 -0500

    got rid of amqp celery backend

--- kaiketsu/celery.py
+++ kaiketsu/celery.py
@@ -6,11 +6,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kaiketsu.settings')
 
 app = Celery('kaiketsu',
              broker='amqp://localhost',
-             backend='amqp://',
              include=['kaiketsu.tasks'])
 
-print(os.environ['CLOUDAMQP_URL'])
-app.conf.update(BROKER_URL=os.environ['CLOUDAMQP_URL'])
+
+app.conf.update(
+    BROKER_URL=os.environ['CLOUDAMQP_URL'],
+    RESULT_BACKEND=None
+    )
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.timezone = 'EST'
 app.autodiscover_tasks()
